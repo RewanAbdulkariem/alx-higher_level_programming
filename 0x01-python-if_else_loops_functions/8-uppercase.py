0debd38bd44aa27d7f869fd9d7e4b421af65c95c
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-06 03:20:06 +0300

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        if ord(c) < 123 and ord(c) > 96:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
+
