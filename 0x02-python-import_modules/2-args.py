790e1b391eed335f68fb421febef81c4578d00e4
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-07 21:11:33 +0300

    add separator

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,9 @@ from sys import argv
 if __name__ == "__main__":
     arg_count = len(argv) - 1
     arg_plural = "argument" if arg_count == 1 else "arguments"
+    arg_separator = ":" if arg_count > 0 else "."
 
-    print("{} {}".format(arg_count, arg_plural))
+    print("{} {}{}".format(arg_count, arg_plural, arg_separator))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
