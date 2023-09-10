27909a94db9b8518e1c315109602e7e4b5030e4b
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-11 01:48:29 +0300

    string. replace is not allowd

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace("C", "")
-    my_string = my_string.replace("c", "")
-    return my_string
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+    return new
