402e6c883b9eb4fc931ed03b2a0b63d6be76d150
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-12 02:23:11 +0300

    try4

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+listint_t *reverseList(listint_t *head);
+/**
+ * is_palindrome - Check if a singly-linked list of integers is a palindrome.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	listint_t *secondHalf = reverseList(slow);
+
+	listint_t *firstHalf = *head;
+
+	while (secondHalf != NULL)
+	{
+		if (firstHalf->n != secondHalf->n)
+			return (0);
+		firstHalf = firstHalf->next;
+		secondHalf = secondHalf->next;
+	}
+	return (1);
+}
 /**
  * reverseList - Reverse a singly-linked list in place.
  * @head: Pointer to the head of the linked list.
@@ -21,36 +54,4 @@ listint_t *reverseList(listint_t *head)
 		current = next;
 	}
 	return (prev);
-}
-/**
- * is_palindrome - Check if a singly-linked list of integers is a palindrome.
- * @head: Pointer to the head of the linked list.
- *
- * Return: 1 if it is a palindrome, 0 otherwise.
- */
-int is_palindrome(listint_t **head)
-{
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-
-	if (!head || !(*head)->next)
-		return (1);
-
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	listint_t *secondHalf = reverseList(slow);
-
-	listint_t *firstHalf = *head;
-
-	while (secondHalf != NULL)
-	{
-		if (firstHalf->n != secondHalf->n)
-			return (0);
-		firstHalf = firstHalf->next;
-		secondHalf = secondHalf->next;
-	}
-	return (1);
 }
