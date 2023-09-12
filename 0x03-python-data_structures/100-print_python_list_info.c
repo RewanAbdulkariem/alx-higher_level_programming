ea6a34fc2a4088264bad458cd65dd667cf20cdef
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-12 21:48:41 +0300

    it compiles and runs in ubuntu 20 checkers don't compilr

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,7 +7,7 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t alloc = ((PyListObject *)p)->allocated;
 	PyObject *obj;
 
-	printf("[*] Size of the Python List = %ld", list_size);
+	printf("[*] Size of the Python List = %ld\n", list_size);
 	printf("[*] Allocated = %ld\n", alloc);
 
 	for (Py_ssize_t i = 0; i < list_size; i++)
