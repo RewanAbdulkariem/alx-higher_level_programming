d425737a2efaca11b98fb972428aad63c32a7af5
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-27 02:43:33 +0300

    task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,9 @@
 
 
 class Square:
-    """Initializes a new square with the given size."""
+    """
+    Initializes a new square with the given size.
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
