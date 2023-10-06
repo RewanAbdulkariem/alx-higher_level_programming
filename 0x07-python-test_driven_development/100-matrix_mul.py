488cd7b4bf07b90adb8dedeccc0af6324fdc4c0f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-06 10:02:42 +0300

    fix from test case7 to 16

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,16 +22,15 @@ def matrix_mul(m_a, m_b):
 
     if not all(isinstance(num, (int, float)) for row in m_a for num in row):
         raise TypeError("m_a should contain only integers or "
-                        "floats or m_b should contain only integers or floats")
+                        "floats")
 
     if not all(isinstance(num, (int, float)) for row in m_b for num in row):
-        raise TypeError("m_a should contain only integers or "
-                        "floats or m_b should contain only integers or floats")
+        raise TypeError("m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(
-               len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_a must be of the same size"
-                        " or each row of m_b must be of the same size")
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_b must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_a must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
