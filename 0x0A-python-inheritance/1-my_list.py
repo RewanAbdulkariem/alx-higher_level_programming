0bb2cb2b85e4ffc43cc1728c1c307a27454284b2
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-09 16:52:46 +0300

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module to class
+"""
+
+class MyList(list):
+    """
+    Custom list class that inherits from the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+        """
+        print(sorted(self))
