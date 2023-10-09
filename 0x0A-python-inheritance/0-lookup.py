d6f0bba242578029f3fbf8a9229cb3d34610d82a
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-09 16:27:39 +0300

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module lookup to find the attributes and methods
+"""
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    """
+    return dir(obj)
