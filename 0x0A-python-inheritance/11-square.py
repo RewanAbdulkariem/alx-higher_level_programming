2b3baa762112fdc6c356ecbf94646683e14b4efa
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-10 16:02:57 +0300

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-   def __str__(self):
+    def __str__(self):
         """
         Returns a string representation of the Square.
 
