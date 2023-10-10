22b9ac2403cca961504adedad5472c66b7700022
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-10 15:50:01 +0300

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module '8-rectangle' contains Rectangle class.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class inherits from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes the Rectangle instance.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle.
+
+        Returns:
+        str: String representation of the Rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
