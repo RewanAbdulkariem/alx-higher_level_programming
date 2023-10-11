3b16d9ec33f54189b79c0a72fb2a5c3885eda22f
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-11 21:20:03 +0300

    add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
+Module '0-read_file' to read a text file and print its content.
 """
 
 
 def read_file(filename=""):
     """
+    Read the content of the specified text file
+    and print it to the standard output.
     """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
-    
