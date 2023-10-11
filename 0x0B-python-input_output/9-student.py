eed5c48d90d7390e3a36aa1bb4b6dcbda29f7ccc
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-12 02:34:10 +0300

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -17,7 +17,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-
     def to_json(self):
         """
         Retrieves a dictionary representation of the Student instance.
