bb7df4dd3ee9c50612f09fef9fcd6b88ed5d3eaa
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-10-13 13:01:59 +0300

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,8 +17,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of the Student instance.
         """
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+
+        json_dict = {}
+        for attribute in attrs:
+            if hasattr(self, attribute):
+                json_dict[attribute] = getattr(self, attribute)
+        return json_dict
