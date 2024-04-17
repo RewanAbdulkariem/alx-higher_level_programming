3dc3ac03ce9d3a8b45380db8a7d79da489e48184
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-17 12:17:36 +0200

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+0-rectangle.py module
+"""
+
+class Rectangle:
+    """
+    An empty class Rectangle that defines a rectangle
+    """
+    def __init__(self):
+        pass
