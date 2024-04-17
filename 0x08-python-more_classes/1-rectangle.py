b7cec6009179aa2cd0eb56f56d8dc818b84dfd5a
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-17 12:32:31 +0200

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+0-rectangle.py module
+"""
+
+
+class Rectangle:
+    """
+    An empty class Rectangle that defines a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+    
+    def width(self):
+        """ property to retrieve it """
+        return Rectangle.__width
+
+    def width(self, value):
+        """property setter to set it"""
+        if isinstance(value, int) and value >= 0:
+            Rectangle.__width = value
+        elif isinstance(value, int) and value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+
+    def height(self):
+        """ property to retrieve it """
+        return Rectangle.__height
+
+    def height(self, value):
+        """property setter to set it"""
+        if isinstance(value, int) and value >= 0:
+            Rectangle.__width = value
+        elif isinstance(value, int) and value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
