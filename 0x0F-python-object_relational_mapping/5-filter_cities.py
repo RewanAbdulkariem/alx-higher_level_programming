a2eb2e843a62a2f27cfc7727626d77633d0e8cc9
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-19 22:50:25 +0200

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Lists cities of a specific state.
+"""
+import MySQLdb
+from sys import argv
+
+
+if len(argv) < 4:
+    print("Usage: {} <username> <password> <database>".format(argv[0]))
+    exit(1)
+
+try:
+    db_connection = MySQLdb.connect(
+     host="localhost",
+     port=3306, user=argv[1],
+     passwd=argv[2],
+     db=argv[3],
+     charset="utf8"
+    )
+
+    cursor = db_connection.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities\
+             INNER JOIN states ON cities.state_id = states.id\
+             ORDER BY id ASC;")
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        if argv[4] in row:
+            print(row[1], end=", ")
+    cursor.close()
+    db_connection.close()
+
+except MySQLdb.Error as e:
+    print("Error connecting to the database:", e)
