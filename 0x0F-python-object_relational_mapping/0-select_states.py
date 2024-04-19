3a3b52410b120b0b971e7ec124b3acb8d0c3b75b
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-19 20:07:21 +0200

    fix style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,16 +11,15 @@ if len(argv) < 4:
     exit(1)
 
 try:
-    db_connection= MySQLdb.connect(
-    host="localhost",
-    port=3306, user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    charset="utf8"
+    db_connection = MySQLdb.connect(
+     host="localhost",
+     port=3306, user=argv[1],
+     passwd=argv[2],
+     db=argv[3],
+     charset="utf8"
     )
 
-
-    cursor=db_connection.cursor()
+    cursor = db_connection.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC;")
     query_rows = cursor.fetchall()
