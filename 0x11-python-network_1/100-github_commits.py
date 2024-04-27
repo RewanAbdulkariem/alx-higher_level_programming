48065fddd3350bc727fcd69fc074afa63387cebf
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 05:44:53 +0300

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+import sys
+import requests
+
+
+if __name__=="__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner_name, repository_name)
+    
+    req = requests.get(url)
+    commits = req.json()
+
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
