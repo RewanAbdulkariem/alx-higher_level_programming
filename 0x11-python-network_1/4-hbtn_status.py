76a05809e6d84b2729d978ffccd2069b08ece2a3
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 04:31:54 +0300

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == '__main__':
     html = requests.get('https://w3schools.com/python/demopage.htm')
     print("Body response:")
-    print("\t- type: {}".format(type(html)))
+    print("\t- type: {}".format(type(html.text)))
     print("\t- content: {}".format(html.text))
