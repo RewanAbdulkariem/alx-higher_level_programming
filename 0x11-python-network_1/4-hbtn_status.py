522b62bb1c2b561381d4dd436a330443ca38e855
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 04:29:31 +0300

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,8 @@
 fetches https://alx-intranet.hbtn.io/status
 """
 import requests
-
-html = requests.get('https://w3schools.com/python/demopage.htm')
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html.text))
+if __name__ == '__main__':
+    html = requests.get('https://w3schools.com/python/demopage.htm')
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html.text))
