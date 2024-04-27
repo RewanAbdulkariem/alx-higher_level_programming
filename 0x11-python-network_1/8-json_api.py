fb85d8da723dbee0087371ae680ceb0b460a434b
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 05:28:17 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,6 +4,23 @@ a Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
+import sys
+import requests
+
 
 if __name__ == "__main__":
-    
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    req = requests.post('http://0.0.0.0:5000/search_user', {'q': letter})
+
+    try:
+        r = req.json()
+        if r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
