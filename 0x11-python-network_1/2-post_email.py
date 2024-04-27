6db344119ede444934a427a47e5592bb5660cf6a
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 03:35:03 +0300

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    params = {'email': sys.argv[2]} 
+
+    req = Request(url, urlencode(params).encode('utf-8'))
+    with urlopen(req) as response:
+        res = response.read().decode('utf-8')
+        print(res)
