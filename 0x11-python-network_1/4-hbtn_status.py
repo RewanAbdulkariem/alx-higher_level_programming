12f8ab40c07b2de4f3e89181e8eff396b8efbd2a
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-27 04:40:09 +0300

    forget to replace the link in the tutorial

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == '__main__':
-    html = requests.get('https://w3schools.com/python/demopage.htm')
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(html.text)))
     print("\t- content: {}".format(html.text))
