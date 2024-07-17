5c7ab710cc56c6816cd41176573bfe875c025ec4
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-18 02:11:42 +0300

    task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module base.py
+"""
+class Base:
+    """
+        Base class for managing id attribute in future classes.
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Initializes an instance of the Base class.
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
