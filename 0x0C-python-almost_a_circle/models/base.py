07b0c88159dee4c0848f42eb81adf39ef4d5e8f1
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-18 04:35:12 +0300

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,6 +52,6 @@ class Base:
         returns the list of the JSON string representation json_string
         """
         if json_string is None:
-            return '[]'
+            return []
         data = json.loads(json_string)
         return data
