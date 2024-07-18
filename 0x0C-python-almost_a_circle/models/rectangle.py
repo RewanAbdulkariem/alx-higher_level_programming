aa28593690ee3bba3756d9e9165c0cab0751ccdf
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-18 03:04:56 +0300

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,10 @@ class Rectangle(Base):
         """
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """
+        returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        return (f'''[Rectangle] ({self.id})
+                 {self.x}/{self.y} - {self.width}/{self.height}''')
