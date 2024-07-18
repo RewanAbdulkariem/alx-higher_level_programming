8f48e2ffc40b90df1a79d2c8be3834c94179b4d3
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-18 03:20:00 +0300

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
         for _ in range(self.y):
             print()
         for _ in range(self.__height):
-            print(' '*self.x, '#' * self.__width)
+            print(' '*self.__x + '#' * self.__width)
 
     def __str__(self):
         """
