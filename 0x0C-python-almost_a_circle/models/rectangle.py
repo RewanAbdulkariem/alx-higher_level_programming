078b9f0d8c3282cd4e6097fdd2b00c8fb9421bef
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-18 03:11:30 +0300

    7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,8 +84,10 @@ class Rectangle(Base):
         """
         prints in stdout the Rectangle instance with the character #
         """
+        for _ in range(self.y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(' '*self.x, '#' * self.__width)
 
     def __str__(self):
         """
